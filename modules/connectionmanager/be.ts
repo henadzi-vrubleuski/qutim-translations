aefb4ae343117f7f42083275d6322acf52424b4b
Aliaksandr Trush <evils.mail@gmail.com>
2012-02-12 22:14:46 +0300

    reate first be_BY translated files

---
+++ modules/connectionmanager/be.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="be_BY">
+<context>
+    <name>Author</name>
+    <message>
+        <source>Sidorov Aleksey</source>
+        <translation type="obsolete">Сидоров Алексей</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectionManager::ConnectionManager</name>
+    <message>
+        <source>within %1 seconds</source>
+        <translatorcomment>Как тут с формами быть, няшка?</translatorcomment>
+        <translation type="obsolete">на працягу %1 секунд</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../../plugins/connectionmanager/src/connectionmanager.cpp" line="+124"/>
+        <source>within %n seconds</source>
+        <comment>reconnect at</comment>
+        <translation type="unfinished">
+            <numerusform>на працягу %n секунды</numerusform>
+            <numerusform>на працягу %n секунд</numerusform>
+            <numerusform>на працягу %n секунд</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>immediately</source>
+        <translation>неадкладна</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>%1 will be reconnected %2</source>
+        <translation>%1 будзе перадалучан %2</translation>
+    </message>
+    <message>
+        <source>ConnectionManager</source>
+        <translation type="obsolete">Мэнэджэр далучэння</translation>
+    </message>
+</context>
+<context>
+    <name>ManagerSettings</name>
+    <message>
+        <location filename="../../plugins/connectionmanager/src/managersettings.ui" line="+14"/>
+        <source>Form</source>
+        <translation>Налады</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Autoconnect on startup</source>
+        <translation>Далучацца пры старце</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>ConnectionManager</source>
+        <translation type="obsolete">Мэнэджэр далучэння</translation>
+    </message>
+    <message>
+        <source>Used to monitor the availability of network.</source>
+        <translation type="obsolete">Выкарыстоўваецца для адсочвання даступнасці сеткі.</translation>
+    </message>
+</context>
+<context>
+    <name>Service</name>
+    <message>
+        <location filename="../../plugins/connectionmanager/src/connectionmanager.cpp" line="-93"/>
+        <source>BearerManager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location line="+15"/>
+        <source>Connection manager</source>
+        <translation>Мэнэджэр далучэння</translation>
+    </message>
+</context>
+<context>
+    <name>Task</name>
+    <message>
+        <source>Author</source>
+        <translation type="obsolete">Аўтар</translation>
+    </message>
+</context>
+</TS>
